6a43a552241e0dbad2d5c4275292490a4cc8e415
Sachin Gupta <sachin070gupta@gmail.com>
2021-02-07 17:17:59 +0530

    Day 2 completed

---
+++ lib/home_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    int days = 30;
+    return Scaffold(
+      appBar: new AppBar(
+        title: Text("30 Days Of Flutter"),
+      ),
+      drawer: Drawer(),
+      body: Center(
+        child: Container(
+          child: Text("Welcome to $days Days of Flutter"),
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'home_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -7,14 +8,6 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Material(
-        child: Center(
-          child: Container(
-            child: Text("Welcome to 30 Days of Flutter"),
-          ),
-        ),
-      ),
-    );
+    return MaterialApp(home: HomePage());
   }
 }
